939a84267a492c6f5e8501653104199154feda42
JPMC <JPMC@192.168.0.106>
2012-02-14 21:56:53 -0600

    Creado proyecto TCarretera con main UICarretera.

---
+++ src/tcarretera/UICarretera.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tcarretera;
+
+/**
+ *
+ * @author Jose P. Medrano
+ * @author  Daniela Brizuela
+ */
+public class UICarretera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
